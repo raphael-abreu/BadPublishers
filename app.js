460902ee488be14d4a5f141a3126d20e80820090
Raphael Abreu <raphael_abreu@outlook.com>
2016-11-02 23:20:56 -0300

    demo

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
